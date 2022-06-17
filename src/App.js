58d2303923d802b4eb0555b51197d215595ebe18
alandev <alancarloscesar@gmail.com>
2022-06-17 14:04:41 -0300

    Iniciando estrutura do projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './app.css'
+
+function App() {
+  return (
+    <main className="Container">
+      <section className='ContainerTitle'>
+        <p>PLANOS</p>
+      </section>
+      <section className='ContainerPlanos'>
+
+      </section>
+    </main>
+  );
+}
+
+export default App;
